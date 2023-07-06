c8f6281e6d5349d9f73477e5902a7f9329cfb885
Jordan McKinney <jordanmmck@gmail.com>
2023-07-06 13:23:19 -0700

    finished

---
+++ src/SanctionsToken.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SanctionsToken is ERC20 {
+    mapping(address => bool) public blacklist;
+    address public admin;
+
+    constructor() ERC20("SanctionsToken", "STX") {
+        admin = msg.sender;
+        this; // do i need this?
+    }
+
+    // not sure how to set balances to non-zero without this f'n...
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, "only admin");
+        _mint(to, amount);
+    }
+
+    function addToBlacklist(address _addr) external {
+        require(msg.sender == admin, "only admin");
+        blacklist[_addr] = true;
+    }
+
+    function removeFromBlacklist(address _addr) external {
+        require(msg.sender == admin, "only admin");
+        blacklist[_addr] = false;
+    }
+
+    function transfer(
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        require(!blacklist[msg.sender], "sender is blacklisted");
+        require(!blacklist[to], "recipient is blacklisted");
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        require(!blacklist[from], "sender is blacklisted");
+        require(!blacklist[to], "recipient is blacklisted");
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+}
---
+++ test/SanctionsToken.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/SanctionsToken.sol";
+
+contract SanctionsTokenTest is Test {
+    SanctionsToken public sanctionsToken;
+    address public alice = address(0x1);
+    address public bob = address(0x2);
+
+    function setUp() public {
+        // ensure admin is set to msg.sender
+        vm.prank(address(0));
+        sanctionsToken = new SanctionsToken();
+
+        vm.prank(address(0));
+        sanctionsToken.mint(alice, 1e18);
+    }
+
+    function testAddToBlacklist() public {
+        address addr = address(0x1);
+        vm.prank(address(0));
+        sanctionsToken.addToBlacklist(addr);
+        assertEq(sanctionsToken.blacklist(addr), true);
+    }
+
+    function testAddToBlacklistFail() public {
+        address addr = address(0x1);
+        vm.prank(address(123));
+        vm.expectRevert();
+        sanctionsToken.addToBlacklist(addr);
+    }
+
+    function testRemoveFromBlacklist() public {
+        address addr = address(0x1);
+        vm.prank(address(0));
+        sanctionsToken.addToBlacklist(addr);
+        assertEq(sanctionsToken.blacklist(addr), true);
+        vm.prank(address(0));
+        sanctionsToken.removeFromBlacklist(addr);
+        assertEq(sanctionsToken.blacklist(addr), false);
+    }
+
+    function testRemoveFromBlacklistFail() public {
+        address addr = address(0x1);
+        vm.prank(address(0));
+        sanctionsToken.addToBlacklist(addr);
+        assertEq(sanctionsToken.blacklist(addr), true);
+        vm.prank(address(0x99));
+        vm.expectRevert();
+        sanctionsToken.removeFromBlacklist(addr);
+    }
+
+    function testTransfer() public {
+        vm.prank(alice);
+        sanctionsToken.transfer(bob, .5e18);
+
+        uint aliceBalance = sanctionsToken.balanceOf(alice);
+        assertEq(aliceBalance, 1e18 - .5e18);
+        uint bobBalance = sanctionsToken.balanceOf(bob);
+        assertEq(bobBalance, .5e18);
+    }
+
+    function testTransferToBlacklist() public {
+        vm.prank(address(0));
+        sanctionsToken.addToBlacklist(bob);
+
+        vm.expectRevert();
+        vm.prank(alice);
+        sanctionsToken.transfer(bob, .5e18);
+    }
+
+    function testTransferFromBlacklist() public {
+        vm.prank(address(0));
+        sanctionsToken.addToBlacklist(alice);
+
+        vm.expectRevert();
+        vm.prank(alice);
+        sanctionsToken.transfer(bob, .5e18);
+    }
+}
